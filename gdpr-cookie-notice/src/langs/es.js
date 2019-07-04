cd0af966d7fcce286caab846761a541682d571ee
Yrjö <39963848+eyrjo@users.noreply.github.com>
2019-07-04 12:01:58 +0200

    Create es.js

---
+++ gdpr-cookie-notice/src/langs/es.js
@@ -0,0 +1,17 @@
+//Add strings
+gdprCookieNoticeLocales.es = {
+  description: 'Usamos cookies para ofrecerte una mejor experiencia y analizar nuestro tráfico. Lee más acerca de cómo utilizamos las cookies y cómo puedes controlarlas en Ajustes. Si continuas navegando, consideramos que aceptas nuestras cookies.'
+  settings: 'Ajustes',
+  accept: 'Aceptar',
+  statement: 'Política de Cookies',
+  save: 'Guardar ajustes',
+  always_on: 'Siempre',
+  cookie_essential_title: 'Cookies esenciales',
+  cookie_essential_desc: 'Son las requeridas para que este sitio funcione.',
+  cookie_performance_title: 'Cookies funcionales',
+  cookie_performance_desc: 'Son las que te proporcionan una mejor experiencia de usuario, pero no son necesarias para que la página funcione.',
+  cookie_analytics_title: 'Cookies de análisis',
+  cookie_analytics_desc: 'Son las que monitorizan el uso y rendimiento de este sitio web.',
+  cookie_marketing_title: 'Cookies de marketing',
+  cookie_marketing_desc: 'Son las que muestran mensajes publicitarios relevantes para tus intereses, aunque nosotros no tenemos publicidad.'
+}
